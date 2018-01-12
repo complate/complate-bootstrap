91dab5f9b1447060b5667022f98856154a500109
Willem van Kerkhof <wvk@consolving.de>
2018-01-12 14:45:26 +0100

    Button groups and toolbars

---
+++ components/button-toolbar/index.jsx
@@ -0,0 +1,13 @@
+import { createElement } from "complate-stream";
+
+export default function ButtonToolbar({ class: additionalClass, ariaLabel}, ...children) {
+	let classNames = ["btn-toolbar"];
+
+	if (additionalClass) {
+		classNames.push(additionalClass);
+	}
+
+	return <div class={classNames.join(" ")} role="toolbar" aria-label={ariaLabel}>
+		{children}
+	</div>;
+}
