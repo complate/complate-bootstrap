b0015878887a6d63b561955d7aa82052f8056874
Willem van Kerkhof <wvk@consolving.de>
2018-01-12 14:00:29 +0100

    Bootstrap 4 styles for buttons

--- components/button/index.jsx
+++ components/button/index.jsx
@@ -1,6 +1,6 @@
 import { createElement } from "complate-stream";
 
-export default function Button({ class: additionalClass, active, disabled, contextual, title, type: typeName, size }, ...children) {
+export default function Button({ class: additionalClass, active, disabled, block, outline, contextual, title, type: typeName, size }, ...children) {
 	let classNames = ["btn"];
 
 	if (additionalClass) {
@@ -15,14 +15,18 @@ export default function Button({ class: additionalClass, active, disabled, conte
 		classNames.push("disabled");
 	}
 
-	if (contextual) {
+	if (contextual && outline) {
+		classNames.push(`btn-outline-${contextual}`);
+	} else if (contextual) {
 		classNames.push(`btn-${contextual}`);
-	} else {
-		classNames.push("btn-default");
 	}
 
 	if (size) {
 		classNames.push(`btn-${size}`);
+	}
+
+	if (block) {
+		classNames.push("btn-block");
 	}
 
 	if (!typeName) {
