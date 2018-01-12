fd5dcf67ae7f12d894e2da28ee4d38528f3379e0
Martin Kühl <martin.kuehl@gmail.com>
2018-01-12 17:53:53 +0100

    Badge: Support additional class attribute

--- components/badge/index.jsx
+++ components/badge/index.jsx
@@ -1,16 +1,18 @@
 import { createElement } from "complate-stream";
 
-export default function Badge({ contextual = "secondary", href, pill }, ...children) {
+export default function Badge({ class: additionalClass, contextual = "secondary", href, pill }, ...children) {
 	let tag = "span";
 	let attrs = {};
 	if(href) {
 		tag = "a";
 		attrs["href"] = href;
 	}
-	let classNames = ["badge"];
-	classNames.push(`badge-${contextual}`);
+	let classNames = ["badge", `badge-${contextual}`];
 	if(pill) {
 		classNames.push("badge-pill");
+	}
+	if(!additionalClass) {
+		classNames.push(additionalClass);
 	}
 	attrs["class"] = classNames.join(" ");
 	return createElement(tag, attrs, children);
