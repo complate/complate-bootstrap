bd9f08735d62d990edfdc9f5786abf159a8faf45
Marc Jansing <marc.jansing@innoq.com>
2019-01-04 11:21:18 +0100

    bump faucet

--- faucet.config.js
+++ faucet.config.js
@@ -1,23 +1,13 @@
 "use strict";
 
-let sass = {
-	manifest: false,
-	bundles: [{
-		entryPoint: "./lib/styles/index.scss",
+module.exports = {
+	watchDirs: ["./lib", "./components"]
+	sass: [{
+		source: "./lib/styles/index.scss",
 		target: "./assets/bundle.css"
-	}]
-};
-
-let js = {
-	manifest: false,
-	bundles: [{
-		entryPoint: "./lib/scripts/index.js",
+	}],
+	js: [{
+		source: "./lib/scripts/index.js",
 		target: "./assets/bundle.js"
 	}]
-};
-
-module.exports = {
-	watchDirs: ["./lib", "./components"],
-	sass,
-	js
 };
